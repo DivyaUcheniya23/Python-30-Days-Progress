54eb1695e64609eec9e67d8dba53c90706845d2a
DivyaUcheniya23 <divya.ucheniya.mail.official@gmail.com>
2024-09-23 04:01:12 +0530

    initial Commit

---
+++ Day1/Day1_basics.py
@@ -0,0 +1,29 @@
+# Python program
+'''
+My first python program'''
+# a = 5ṇ
+# b = 10
+# c = a+b
+# print("a + b - ",c)
+
+# a = input("Enter Some Value\n")
+# print(type(a))
+# a = int(input("Enter Some Value\n"))
+# if a%2==0:
+#     print("number is even : ",a)
+# else:
+#     print("number is odd",a)
+
+# #tables
+# a= int(input("Enter Some Value\n"))
+# for i in range(1,11):
+#     print(i*a)
+
+# fruits = {"apple","banana","orange"} #list--array
+# # print(fruits[2])
+# print(fruits)
+# print(type(fruits))
+fruits = ("apple","banana","orange") #list--array
+print(fruits[2])
+print(fruits)
+print(type(fruits))
